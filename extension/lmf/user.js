d91409be7fe1d7b09bad644cb05b333a53d2e51c
Jakob Frank <jakob.frank@salzburgresearch.at>
2012-07-04 15:45:09 +0200

    first version of extension/lmf/user.js

---
+++ extension/lmf/user.js
@@ -0,0 +1,42 @@
+function UserService(editor, host) {
+
+    function update(ed, concepts, property, callback) {
+        if (!ed.active) return;
+        if (!ed.graph) return;
+
+        var urlParams="?property="+encodeURIComponent(property)
+            + "&context="+encodeURIComponent(ed.graph);
+
+        $.post(ed.host + "skos/currentUser"+urlParams, {uri: concepts})
+            .error(function() { ed.active = false; })
+            .complete(function() { if (callback) callback(); });
+    }
+
+    return {
+        editor: editor,
+        host: host,
+        graph: null,
+        active: true;
+        init: function() {
+            var self = this;
+            self.editor.event.bind(editor.EventCode.GRAPH.LOAD, function(event) {
+                self.graph = event.data.uri;
+            });
+            self.editor.event.bind(editor.EventCode.CONCEPT.UPDATED, function(event) {
+                var uris = event.data.uris;
+                var prop = "dc:contributor";
+                update(self, uris, prop, function() {
+                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, prop);
+                });
+            });
+            self.editor.event.bind(editor.EventCode.CONCEPT.CRETED, function(event) {
+                var uris = event.data.uris;
+                var prop = "dc:creator";
+                update(self, uris, prop, function() {
+                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, prop);
+                });
+            });
+        }
+    };
+}
+
