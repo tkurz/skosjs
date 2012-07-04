f03391764c4b046eed5d08c9a0518d469e49b802
Jakob Frank <jakob.frank@salzburgresearch.at>
2012-07-04 17:03:21 +0200

    first implementation for lmf-user extension in skos-js

--- extension/lmf/user.js
+++ extension/lmf/user.js
@@ -1,11 +1,14 @@
 function UserService(editor, host) {
 
-    function update(ed, concepts, property, callback) {
+    function update(ed, concepts, property, single, callback) {
         if (!ed.active) return;
         if (!ed.graph) return;
 
+        var delExisting = (single == true);
+
         var urlParams="?property="+encodeURIComponent(property)
-            + "&context="+encodeURIComponent(ed.graph);
+            + "&context="+encodeURIComponent(ed.graph)
+            + "&replace="+delExisting;
 
         $.post(ed.host + "skos/currentUser"+urlParams, {uri: concepts})
             .error(function() { ed.active = false; })
@@ -19,22 +22,34 @@ function UserService(editor, host) {
         active: true,
         init: function() {
             var self = this;
+            function handleUpdate(event) {
+                var uris = event.data.uris;
+                var prop = "http://purl.org/dc/elements/1.1/contributor";
+                update(self, uris, prop, false, function() {
+                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, {uris: [prop]});
+                });
+            }
+            function handleCreate(event) {
+                var uris = event.data.uri;
+                var prop = "http://purl.org/dc/elements/1.1/creator";
+                update(self, uris, prop, true, function() {
+                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, {uris: [prop]});
+                });
+            }
+
             self.editor.event.bind(editor.EventCode.GRAPH.LOAD, function(event) {
                 self.graph = event.data.uri;
             });
-            self.editor.event.bind(editor.EventCode.CONCEPT.UPDATED, function(event) {
-                var uris = event.data.uris;
-                var prop = "dc:contributor";
-                update(self, uris, prop, function() {
-                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, prop);
-                });
-            });
-            self.editor.event.bind(editor.EventCode.CONCEPT.CRETED, function(event) {
-                var uris = event.data.uris;
-                var prop = "dc:creator";
-                update(self, uris, prop, function() {
-                    self.editor.event.fire(editor.EventCode.VIEW.RELOAD, prop);
-                });
+
+            self.editor.event.bind(editor.EventCode.CONCEPT.UPDATED, handleUpdate);
+            self.editor.event.bind(editor.EventCode.SCHEME.UPDATED, handleUpdate);
+            self.editor.event.bind(editor.EventCode.GRAPH.UPDATED, handleUpdate);
+
+            self.editor.event.bind(editor.EventCode.CONCEPT.CREATED, handleCreate);
+            self.editor.event.bind(editor.EventCode.SCHEME.CREATED, handleCreate);
+            self.editor.event.bind(editor.EventCode.GRAPH.CREATED, function(event) {
+                self.graph = event.data.uri;
+                handleCreate(event);
             });
         }
     };
--- skos-editor.0.2.0.js
+++ skos-editor.0.2.0.js
@@ -40,10 +40,10 @@ function SKOSEditor(options) {
                 {title:"Description",property:"http://purl.org/dc/elements/1.1/description",type:"text",multivalue:false,multilingual:true,editable:true}
             ],
             right:[
-                {title:"Author",property:"http://purl.org/dc/elements/1.1/author",type:"string",multivalue:true,multilingual:false,editable:true},
+                {title:"Author",property:"http://purl.org/dc/elements/1.1/creator",type:"uri",multivalue:false,multilingual:false,editable:false},
                 {title:"Created",property:"http://purl.org/dc/terms/created",type:"string",multivalue:false,multilingual:false,editable:false},
                 {title:"Publisher (Organisation)",property:"http://purl.org/dc/elements/1.1/publisher",type:"string",multivalue:true,multilingual:false,editable:true},
-                {title:"Contributor",property:"http://purl.org/dc/elements/1.1/contributor",type:"string",multivalue:true,multilingual:false,editable:true}
+                {title:"Contributors",property:"http://purl.org/dc/elements/1.1/contributor",type:"uri",multivalue:true,multilingual:false,editable:false}
             ]
         },
         scheme : {
@@ -53,11 +53,11 @@ function SKOSEditor(options) {
             right:[
                 {title:"Title",property:"http://www.w3.org/2000/01/rdf-schema#label",type:"string",multivalue:false,multilingual:true,editable:true},
                 {title:"Description",property:"http://purl.org/dc/elements/1.1/description",type:"text",multivalue:false,multilingual:true,editable:true},
-                {title:"Author",property:"http://purl.org/dc/elements/1.1/author",type:"string",multivalue:true,multilingual:false,editable:true},
+                {title:"Author",property:"http://purl.org/dc/elements/1.1/creator",type:"uri",multivalue:false,multilingual:false,editable:false},
                 {title:"Created",property:"http://purl.org/dc/terms/created",type:"string",multivalue:false,multilingual:false,editable:false},
                 {title:"Modified",property:"http://purl.org/dc/terms/modified",type:"string",multivalue:false,multilingual:false,editable:false},
                 {title:"Publisher (Organisation)",property:"http://purl.org/dc/elements/1.1/publisher",type:"string",multivalue:true,multilingual:false,editable:true},
-                {title:"Contributor",property:"http://purl.org/dc/elements/1.1/contributor",type:"string",multivalue:true,multilingual:false,editable:true},
+                {title:"Contributors",property:"http://purl.org/dc/elements/1.1/contributor",type:"uri",multivalue:true,multilingual:false,editable:false},
                 {title:"License",property:"http://purl.org/dc/terms/license",type:"uri",multivalue:true,droppable:false,editable:true}
             ]
         },
@@ -75,8 +75,10 @@ function SKOSEditor(options) {
                 {title:"Preferred Label",property:"http://www.w3.org/2004/02/skos/core#prefLabel",type:"string",multivalue:false,multilingual:true,editable:true},
                 {title:"Alternative Label",property:"http://www.w3.org/2004/02/skos/core#altLabel",type:"string",multivalue:true,multilingual:true,editable:true},
                 {title:"Hidden Label",property:"http://www.w3.org/2004/02/skos/core#hiddenLabel",type:"string",multivalue:true,multilingual:true,editable:true},
+                {title:"Author",property:"http://purl.org/dc/elements/1.1/creator",type:"uri",multivalue:false,multilingual:false,editable:false},
                 {title:"Created",property:"http://purl.org/dc/terms/created",type:"string",multivalue:false,multilingual:false,editable:false},
                 {title:"Modified",property:"http://purl.org/dc/terms/modified",type:"string",multivalue:false,multilingual:false,editable:false},
+                {title:"Contributors",property:"http://purl.org/dc/elements/1.1/contributor",type:"uri",multivalue:true,multilingual:false,editable:false},
                 {title:"Definition",property:"http://www.w3.org/2004/02/skos/core#definition",type:"text",multivalue:false,multilingual:true,editable:true},
                 {title:"Hidden Label",property:"http://www.w3.org/2004/02/skos/core#hiddenLabel",type:"string",multivalue:true,multilingual:true,editable:true}
             ]
@@ -1922,6 +1924,7 @@ function SKOSEditor(options) {
                     var uri = OPTIONS.BASE_URI + String.random(8);
                     var title = $("#popup_input").val();
                     skos.create.graph(uri, title, function() {
+                        events.fire(new Event(EventCode.GRAPH.CREATED, {uri:uri}));
                         events.fire(new Event(EventCode.GRAPH.SELECTED, {uri:uri}));
                         close();
                     }, function() {
